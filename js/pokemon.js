e3611a2f839e77951d55e945602424bffb781106
Derek Villars and Jiwon Kang <derek@>
2017-03-16 10:35:57 -0700

    add id to the output

--- js/pokemon.js
+++ js/pokemon.js
@@ -4,7 +4,7 @@ function Pokemon() {
 Pokemon.prototype.getPokemon = function(input) {
   console.log(input);
   $.get('http://pokeapi.co/api/v2/pokemon/' + input).then(function(response) {
-    $('#display-result').html("Name -> " + response.forms[0].name + "<br>Ability ->" + response.abilities[0].ability.name + "<br>Sprite -><img src='" + response.sprites["front_default"] + "'/>");
+    $('#display-result').html("Id -> " + response.id + "<br>Name -> " + response.forms[0].name + "<br>Ability -> " + response.abilities.ability.name + "<br>Sprite -><img src='" + response.sprites["front_default"] + "'/>");
   }).fail(function(error) {
     $("#display-result").text(error.responseJSON.message);
   });
