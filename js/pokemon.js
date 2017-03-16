8ff2e00d9d5bf84f6db8175523f529cf751be442
Derek Villars and Jiwon Kang <derek@>
2017-03-16 11:20:08 -0700

    add working type _grabber_?

--- js/pokemon.js
+++ js/pokemon.js
@@ -4,7 +4,12 @@ function Pokemon() {
 Pokemon.prototype.getPokemon = function(input) {
   console.log(input);
   $.get('http://pokeapi.co/api/v2/pokemon/' + input).then(function(response) {
-    $('#display-result').html("Id -> " + response.id + "<br>Name -> " + response.forms[0].name + "<br>Ability -> " + response.abilities[0].ability.name + "<br>Sprite -><img src='" + response.sprites["front_default"] + "'/>");
+    var newTypes = "";
+    for (var i = 0; i < response.types.length; i++) {
+      var currentType = response.types[i]["type"].name;
+      newTypes = currentType + ", " + newTypes;
+    }
+    $('#display-result').html("POKe´DEX idNo -> " + response.id + "<br>Name -> " + response.forms[0].name + "<br>Types -> " + newTypes + "<br>Ability -> " + response.abilities[0].ability.name + "<br>Sprite -><img src='" + response.sprites["front_default"] + "'/>");
   }).fail(function(error) {
     $("#display-result").text(error.responseJSON.message);
   });
